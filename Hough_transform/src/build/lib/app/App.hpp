bab9e4fe2bf9942bffd4f71ed12f8d6414ba2ec3
SetroZ <yousfsbry66@gmail.com>
2024-11-29 15:36:21 +0800

    file name changes

--- Hough_transform/src/build/lib/app/App.hpp
+++ Hough_transform/src/build/lib/app/App.hpp
@@ -7,7 +7,7 @@
 #include <vector>
 #include <chrono>
 
-#include "../linearTransformer/LinearHoughTransformer.hpp"
+#include "../LinearTransformer/LinearHoughTransformer.hpp"
 #include "../regriding/LinearToSquareRegrider.hpp"
 #include "../visualSpace/VisualSpace.hpp"
 #include "../stopwatch/StopWatch.hpp"
@@ -18,19 +18,17 @@ using namespace CCfits;
 
 namespace fs = filesystem;
 
-int main(int argc, char*argv[]);
-vector<fs::path>* attainAllFITSFilePathsInDirectory(string directoryPath);
+int main(int argc, char *argv[]);
+vector<fs::path> *attainAllFITSFilePathsInDirectory(string directoryPath);
 double convertDmToTheta(double dm);
-bool isFitsFile(const fs::directory_entry& file);
+bool isFitsFile(const fs::directory_entry &file);
 fs::path deturmineResultsPathForSource(fs::path source);
 void ensurePathExists(fs::path path);
 void searchForFrbInFile(fs::path source_path, fs::path result_path);
-double readKey(PHDU& fits, const string& key);
+double readKey(PHDU &fits, const string &key);
 int calculateDmBucketCount(double d_time, double min_disp, double max_disp, double min_freq, double max_freq);
 double calculateD_dm(double d_time, double max_disp, double min_freq, double max_freq);
 void logProcessingFitsFileStart(string fileName);
-
-
 
 #define FITSSOURCEFOLDER "./fitsFiles"
 #define FITSRESULTSFOLDER "./results"
--- Hough_transform/src/build/lib/regriding/LinearToSquareRegrider.hpp
+++ Hough_transform/src/build/lib/regriding/LinearToSquareRegrider.hpp
@@ -1,33 +1,33 @@
 #pragma once
 
-#define K_DM 4.148*1000
+#define K_DM 4.148 * 1000
 
 #define _USE_MATH_DEFINES
 #include <math.h>
 
-#include "../VisualSpace/VisualSpace.hpp"
+#include "../visualSpace/VisualSpace.hpp"
 
 class LinearToSquareRegrider
 {
-    private:
-        VisualSpace* originalSpace;
-        VisualSpace* regridedSpace;
-        int regrid_value;
-        float y_max;
-        float y_min;
-        bool delete_original_space_on_finish;
-    
-    public:
-        LinearToSquareRegrider(VisualSpace* source_space, int regrid_value, float y_min, float y_max, bool delete_original_space_when_deleted);
-        ~LinearToSquareRegrider();
-        VisualSpace* data();
-        double dmToTheta(double dm, int number_of_frequencies, double time_step);
-        double thetaToDm(double theta, int number_of_frequencies, double time_step);
+private:
+    VisualSpace *originalSpace;
+    VisualSpace *regridedSpace;
+    int regrid_value;
+    float y_max;
+    float y_min;
+    bool delete_original_space_on_finish;
 
-    private:
-        void regrid_space();
-        VisualSpace* generateExpandedImage(int, int);
-        VisualSpace* preformRegridingAcrossTempArrays(VisualSpace*);
-        void projectTempArrayOntoRegridedSpace(VisualSpace*);
-        int calculateRegridedY(int, double);
+public:
+    LinearToSquareRegrider(VisualSpace *source_space, int regrid_value, float y_min, float y_max, bool delete_original_space_when_deleted);
+    ~LinearToSquareRegrider();
+    VisualSpace *data();
+    double dmToTheta(double dm, int number_of_frequencies, double time_step);
+    double thetaToDm(double theta, int number_of_frequencies, double time_step);
+
+private:
+    void regrid_space();
+    VisualSpace *generateExpandedImage(int, int);
+    VisualSpace *preformRegridingAcrossTempArrays(VisualSpace *);
+    void projectTempArrayOntoRegridedSpace(VisualSpace *);
+    int calculateRegridedY(int, double);
 };
